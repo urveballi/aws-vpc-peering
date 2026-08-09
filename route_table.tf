4c1b2a67fc6b6ef7c53152521de2abb0d61ffc22
urveballi <urve1996@gmail.com>
2026-08-09 12:32:36 -0400

    routes has been added

--- route_table.tf
+++ route_table.tf
@@ -40,20 +40,62 @@ resource "aws_route_table" "tertiary_rt" {
   }
 }
 
+resource "aws_route" "primary_to_secondary" {
+  route_table_id            = aws_route_table.primary_rt.id
+  destination_cidr_block    = var.secondary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+  provider                  = aws.primary
+}
+
+resource "aws_route" "primary_to_tertiary" {
+  route_table_id            = aws_route_table.primary_rt.id
+  destination_cidr_block    = var.tertiary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_tertiary.id
+  provider                  = aws.primary
+}
+
+resource "aws_route" "secondary_to_primary" {
+  route_table_id            = aws_route_table.secondary_rt.id
+  destination_cidr_block    = var.primary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+  provider                  = aws.secondary
+}
+
+resource "aws_route" "secondary_to_tertiary" {
+  route_table_id            = aws_route_table.secondary_rt.id
+  destination_cidr_block    = var.tertiary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.secondary_to_tertiary.id
+  provider                  = aws.secondary
+}
+
+resource "aws_route" "tertiary_to_primary" {
+  route_table_id            = aws_route_table.tertiary_rt.id
+  destination_cidr_block    = var.primary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_tertiary.id
+  provider                  = aws.secondary
+}
+
+resource "aws_route" "tertiary_to_secondary" {
+  route_table_id            = aws_route_table.tertiary_rt.id
+  destination_cidr_block    = var.secondary_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.secondary_to_tertiary.id
+  provider                  = aws.secondary
+}
+
 resource "aws_route_table_association" "primary_rta" {
-  subnet_id      = aws_subnet.primary_subnet.id
+  subnet_id      = aws_subnet.primary.id
   route_table_id = aws_route_table.primary_rt.id
   provider       = aws.primary
 }
 
 resource "aws_route_table_association" "secondary_rta" {
-  subnet_id      = aws_subnet.secondary_subnet.id
+  subnet_id      = aws_subnet.secondary.id
   route_table_id = aws_route_table.secondary_rt.id
   provider       = aws.secondary
 }
 
 resource "aws_route_table_association" "tertiary_rta" {
-  subnet_id      = aws_subnet.tertiary_subnet.id
+  subnet_id      = aws_subnet.tertiary.id
   route_table_id = aws_route_table.tertiary_rt.id
   provider       = aws.secondary
 }
