91a123f43bf239423080af28b3a87c349a19c93d
urveballi <urve1996@gmail.com>
2026-08-09 12:56:37 -0400

    VPC peering has been completed

---
+++ vpc_peering.tf
@@ -0,0 +1,54 @@
+resource "aws_vpc_peering_connection" "primary_to_secondary" {
+  peer_vpc_id = aws_vpc.secondary.id
+  vpc_id      = aws_vpc.primary.id
+  auto_accept = false
+  peer_region = var.secondary
+  provider     = aws.primary
+
+  tags = {
+    Name = "primary-to-secondary-peering"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "secondary_accept" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
+  auto_accept               = true
+  provider                  = aws.secondary
+}
+
+resource "aws_vpc_peering_connection" "primary_to_tertiary" {
+  peer_vpc_id = aws_vpc.tertiary.id
+  vpc_id      = aws_vpc.primary.id
+  auto_accept = false
+  peer_region = var.tertiary
+  provider     = aws.primary
+
+  tags = {
+    Name = "primary-to-tertiary-peering"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "tertiary_accept_from_primary" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_tertiary.id
+  auto_accept               = true
+  provider                  = aws.secondary
+}
+
+resource "aws_vpc_peering_connection" "secondary_to_tertiary" {
+  peer_vpc_id = aws_vpc.tertiary.id
+  vpc_id      = aws_vpc.secondary.id
+  auto_accept = false
+  peer_region = var.tertiary
+  provider     = aws.secondary
+
+  tags = {
+    Name = "secondary-to-tertiary-peering"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "tertiary_accept_from_secondary" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.secondary_to_tertiary.id
+  auto_accept               = true
+  provider                  = aws.secondary
+}
+
