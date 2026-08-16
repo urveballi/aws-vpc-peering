1e3276a8c8ad0b18c8489d82721b0aa32cbeb8d0
urveballi <urve1996@gmail.com>
2026-08-15 20:42:56 -0400

    variables has been updated

--- variables.tf
+++ variables.tf
@@ -42,3 +42,57 @@ variable "tertiary_vpc_cidr" {
     type = string
     default = "192.168.0.0/16"
 }
+
+variable "primary_subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "secondary_subnet_cidr" {
+  type        = string
+  default     = "10.1.1.0/24"
+}
+
+variable "tertiary_subnet_cidr" {
+  type        = string
+  default = "192.168.1.0/24"
+}
+
+variable "primary_sg_name" {
+    type = string
+    default = "primary-sg"
+}
+
+variable "secondary_sg_name" {
+    type = string
+    default = "secondary-sg"
+}
+
+variable "tertiary_sg_name" {
+    type = string
+    default = "tertiary-sg"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "primary_key_name" {
+  description = "Name of the SSH key pair for Primary VPC instance (us-east-1)"
+  type        = string
+  default     = ""
+}
+
+variable "secondary_key_name" {
+  description = "Name of the SSH key pair for Secondary VPC instance (us-west-2)"
+  type        = string
+  default     = ""
+}
+
+variable "tertiary_key_name" {
+  description = "Name of the SSH key pair for Tertiary VPC instance (us-west-2)"
+  type        = string
+  default     = ""
+}
